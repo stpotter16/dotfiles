09469f53d0c03d0740874d55c6e716e63d66aef3
Sam Potter <spotter1642@gmail.com>
2023-01-10 05:06:42 -0600

    Add sweet remaps

--- nvim/lua/spotter/remap.lua
+++ nvim/lua/spotter/remap.lua
@@ -3,3 +3,21 @@ vim.g.mapleader = " "
 -- Evenutally, can use the vim.cmd.Ex syntax (post 0.8.0)...or just build from scratch
 vim.keymap.set("n", "<leader>pv", "<cmd>Ex<cr>")
 
+-- Let movement drag text in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Keep the cursor centered in the screen while moving
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Keep cursor centered while searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Yank to system buffer
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+
+-- TODO: Add remap of <C-f> to tmux-sessionizer
