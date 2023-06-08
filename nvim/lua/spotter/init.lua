fcf0661540e3804637b339b97374e6f9664327bc
Sam Potter <spotter1642@gmail.com>
2023-06-08 15:41:47 -0500

    Add autocommand for white space trim

--- nvim/lua/spotter/init.lua
+++ nvim/lua/spotter/init.lua
@@ -1,2 +1,13 @@
 require("spotter.set")
 require("spotter.remap")
+
+local augroup = vim.api.nvim_create_augroup
+local spotterGroup = augroup('spotter', {})
+
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd({"BufWritePre"}, {
+    group = spotterGroup,
+    pattern = "*",
+    command = [[s/\s\+$//e]],
+})
