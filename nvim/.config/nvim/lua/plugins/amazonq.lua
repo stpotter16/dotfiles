20a0c4f9467421feca0b21d6f266efe86da51a46
Sam Potter <spotter@huntenergy.com>
2025-09-05 09:36:16 -0400

    Add amazonq plugin

---
+++ nvim/.config/nvim/lua/plugins/amazonq.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    name = 'amazonq',
+    url = 'https://github.com/awslabs/amazonq.nvim.git',
+    opts = {
+      ssoStartUrl = 'https://d-90674d607c.awsapps.com/start',
+    },
+  },
+}
